d6904b4c80290297d9ef601ec7290f715a54a3f2
Cayo Legal <cayolegal@hotmail.com>
2024-09-23 18:38:41 -0400

    feat(key_press): add key press event to add todo

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTodoStore } from "@/store";
+import { useDebounce } from "@/hooks/useDebounce";
 import { createTodo } from "@/helpers/todo-helpers";
 
 const TodoInput = () => {
   const [text, setText] = useState("");
+  const debouncedText = useDebounce(text, 300);
   const addTodo = useTodoStore((state) => state.addTodo);
 
+  const isValid = debouncedText.length > 0;
+
   const onSubmit = () => {
-    const todo = createTodo(text);
+    const todo = createTodo(debouncedText);
     addTodo(todo);
     setText("");
   };
+
+  useEffect(() => {
+    const onKeyEnter = (event: KeyboardEvent) => {
+      if (event.key === "Enter" && isValid) {
+        onSubmit();
+      }
+    };
+
+    document.addEventListener("keypress", onKeyEnter);
+    return () => document.removeEventListener("keypress", onKeyEnter);
+  }, [debouncedText]);
 
   return (
     <div className="mb-4 flex flex-wrap justify-center items-center gap-2">
@@ -24,7 +39,7 @@ const TodoInput = () => {
       <button
         className="bg-blue-500 text-white p-2 rounded ml-2"
         onClick={onSubmit}
-        disabled={!(text.length > 0)}
+        disabled={!isValid}
       >
         Agregar
       </button>
